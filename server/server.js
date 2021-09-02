87ad5d17fcbf3ca49fb7f7be154c0c2f67adf4de
lukmanmemon <memon_lukman@hotmail.com>
2021-09-01 23:49:28 -0400

    Updates to production/build

--- server/server.js
+++ server/server.js
@@ -14,5 +14,16 @@ app.use(express.json());
 const usersRouter = require('./routes/users');
 app.use('/', usersRouter);
 
+if (process.env.NODE_ENV === 'production') {
+    // Exprees will serve up production assets
+    app.use(express.static('client/build'));
+  
+    // Express serve up index.html file if it doesn't recognize route
+    const path = require('path');
+    app.get('*', (req, res) => {
+      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+}
+
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => console.log('Server listening on ' + PORT));
